c273ac06eaaa6eb5f65a01f588059c78344a4d97
Muhammad Qasim Ashfaq <limitoppo@gmail.com>
2026-04-17 12:49:34 +0500

    Updated

--- Artificial Intelligence LAB-TASK 8/LAB TASK 8.py
+++ Artificial Intelligence LAB-TASK 8/LAB TASK 8.py
@@ -1,18 +1,18 @@
 import math
 
-def minimax(curDepth, nodeIndex, maxTurn, scores, targetDepth):
-    if (curDepth == targetDepth):
-        return scores[nodeIndex]
+def Min_Max(Current_Depth, Node, Max_Turn, Scores, Target_Depth):
+    if (Current_Depth == Target_Depth):
+        return Scores[Node]
 
-    if (maxTurn):
-        return max(minimax(curDepth + 1, nodeIndex * 2, False, scores, targetDepth),
-                    minimax(curDepth + 1, nodeIndex * 2 + 1, False, scores, targetDepth))
+    if (Max_Turn):
+        return max(Min_Max(Current_Depth + 1, Node * 2, False, Scores, Target_Depth),
+                    Min_Max(Current_Depth + 1, Node * 2 + 1, False, Scores, Target_Depth))
     else:
-        return min(minimax(curDepth + 1, nodeIndex * 2, True, scores, targetDepth),
-                    minimax(curDepth + 1, nodeIndex * 2 + 1, True, scores, targetDepth))
+        return min(Min_Max(Current_Depth + 1, Node * 2, True, Scores, Target_Depth),
+                    Min_Max(Current_Depth + 1, Node * 2 + 1, True, Scores, Target_Depth))
 
-scores = [3, 5, 2, 9, 3, 5, 2, 9]
-treeDepth = math.log(len(scores), 2)
+Scores = [3, 5, 2, 9, 7, 4, 1, 8, 6]
+Depth_Of_Tree = math.log(len(Scores), 2)
 print("The optimal value is: ", end = "")
-print(minimax(0, 0, True, scores, treeDepth))
+print(Min_Max(0, 0, True, Scores, Depth_Of_Tree))
 
